c71b9bb0cb9f13dee50b35c4a34c7f67457b7f13
tonkatawe <vlahov.anton@gmail.com>
2020-03-16 16:46:04 +0200

    C# Advanced

---
+++ Advanced/Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._Key_Revolver
+{
+    class Program
+    {
+        //60/100 I asked in softuni.bg forum to support me
+        static void Main(string[] args)
+        {
+            var bulletPrice = int.Parse(Console.ReadLine());
+            var sizeGunBarrel = int.Parse(Console.ReadLine());
+            var bullets = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var locks = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var valueOfIntellegence = int.Parse(Console.ReadLine());
+
+            var bulletStack = new Stack<int>(bullets); //going through the bullet back-to-front. In this case use LIFO
+            var locksQueue = new Queue<int>(locks); //shoot the lock from front-to-back. In this case use FIFO
+            var usedBullets = 0; //it is for counting of used bullets
+            var barrelCount = 0; //it is for checking when Sam need to reload
+            while (true)
+            {
+                if (bulletStack.Any() && locksQueue.Any())
+                {
+                    if (bulletStack.Peek() <= locksQueue.Peek()) //First Case if the bullet <= lock
+                    {
+                        barrelCount++;
+                        usedBullets++;
+                        Console.WriteLine("Bang!");
+                        locksQueue.Dequeue();  //remove the lock
+                        bulletStack.Pop(); //remove the bullet
+                        if (barrelCount == sizeGunBarrel && bulletStack.Any()) 
+                        {
+                            Console.WriteLine("Reloading!");
+                            barrelCount = 0;
+                        }
+                    }
+                    else if (bulletStack.Any())
+                    {
+                        usedBullets++;
+                        barrelCount++;
+                        Console.WriteLine("Ping!");
+                        bulletStack.Pop();
+                    }
+
+                }
+                else if (!bulletStack.Any() && locksQueue.Any())
+                {
+                    Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count}");
+                    break;
+                }
+                else if (bulletStack.Any() && !locksQueue.Any())
+                {
+                    var earn = valueOfIntellegence - (usedBullets * bulletPrice);
+                    Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${earn}");
+                    break;
+                }
+                else if (!bulletStack.Any() && !locksQueue.Any())
+                {
+                    var earn = valueOfIntellegence - (usedBullets * bulletPrice);
+                    Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${earn}");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Advanced/Stacks and Queues - Exercise/12. Cups and Bottles/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _12._Cups_and_Bottles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Console.ReadLine().Split().Select(int.Parse).ToArray(); //input for cup's capacity
+            var j = Console.ReadLine().Split().Select(int.Parse).ToArray(); //input for filled bottle
+            var cupsCapacity = new Queue<int>(n); //cups capacity use FIFO following instruction
+            var filledBottles = new Stack<int>(j); //Filled bottle use LIFO following instruction
+
+            var wasteWater = 0;
+            while (true)
+            {
+                if (cupsCapacity.Any() && filledBottles.Any())
+                {
+                    if (filledBottles.Peek() >= cupsCapacity.Peek())
+                    {
+                        wasteWater += filledBottles.Pop() - cupsCapacity.Dequeue();
+                    }
+                    else if (cupsCapacity.Peek() > filledBottles.Peek())
+                    {
+                        var currentCup = cupsCapacity.Peek();
+                        while (currentCup > 0)
+                        {
+                            currentCup -= filledBottles.Pop();
+                        }
+
+                        wasteWater += Math.Abs(currentCup);
+
+                        cupsCapacity.Dequeue();
+                    }
+                }
+
+                if (!cupsCapacity.Any() && filledBottles.Any())
+                {
+                    Console.WriteLine($"Bottles: {string.Join(" ", filledBottles)}");
+                    Console.WriteLine($"Wasted litters of water: {wasteWater}");
+                    break;
+                }
+                else if (cupsCapacity.Any() && !filledBottles.Any())
+                {
+                    Console.WriteLine($"Cups: {string.Join(" ", cupsCapacity)}");
+                    Console.WriteLine($"Wasted litters of water: {wasteWater}");
+                    break;
+                }
+
+            }
+        }
+    }
+}
