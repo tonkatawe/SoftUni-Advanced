a9c46a852c43f319aa9dd5d892855189915df952
tonkatawe <vlahov.anton@gmail.com>
2020-03-18 08:38:57 +0200

    C# Advanced

--- Advanced/Stacks and Queues - Exercise/11. Key Revolver/Program.cs
+++ Advanced/Stacks and Queues - Exercise/11. Key Revolver/Program.cs
@@ -18,49 +18,38 @@ namespace _11._Key_Revolver
             var locksQueue = new Queue<int>(locks); //shoot the lock from front-to-back. In this case use FIFO
             var usedBullets = 0; //it is for counting of used bullets
             var barrelCount = 0; //it is for checking when Sam need to reload
-            while (true)
+            while (bulletStack.Any() && locksQueue.Any())
             {
-                if (bulletStack.Any() && locksQueue.Any())
+                if (bulletStack.Peek() <= locksQueue.Peek()) //First Case if the bullet <= lock
                 {
-                    if (bulletStack.Peek() <= locksQueue.Peek()) //First Case if the bullet <= lock
-                    {
-                        barrelCount++;
-                        usedBullets++;
-                        Console.WriteLine("Bang!");
-                        locksQueue.Dequeue();  //remove the lock
-                        bulletStack.Pop(); //remove the bullet
-                        if (barrelCount == sizeGunBarrel && bulletStack.Any())
-                        {
-                            Console.WriteLine("Reloading!");
-                            barrelCount = 0;
-                        }
-                    }
-                    else if (bulletStack.Any())
-                    {
-                        usedBullets++;
-                        barrelCount++;
-                        Console.WriteLine("Ping!");
-                        bulletStack.Pop();
-                    }
+                    Console.WriteLine("Bang!");
+                    locksQueue.Dequeue();  //remove the lock
+                    bulletStack.Pop(); //remove the bullet
+                }
+                else
+                {
+                    Console.WriteLine("Ping!");
+                    bulletStack.Pop();
+                }
+                barrelCount++;
 
-                }
-                else if (!bulletStack.Any() && locksQueue.Any())
+                if (barrelCount == sizeGunBarrel && bulletStack.Any())
                 {
-                    Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count}");
-                    break;
+                    Console.WriteLine("Reloading!");
+                    barrelCount = 0;
                 }
-                else if (bulletStack.Any() && !locksQueue.Any())
-                {
-                    var earn = valueOfIntellegence - (usedBullets * bulletPrice);
-                    Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${earn}");
-                    break;
-                }
-                else if (!bulletStack.Any() && !locksQueue.Any())
-                {
-                    var earn = valueOfIntellegence - (usedBullets * bulletPrice);
-                    Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${earn}");
-                    break;
-                }
+                usedBullets++;
+            }
+            if (bulletStack.Count >= locksQueue.Count)
+            {
+                var earn = valueOfIntellegence - (usedBullets * bulletPrice);
+                Console.WriteLine($"{bulletStack.Count} bullets left. Earned ${earn}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't get through. Locks left: {locksQueue.Count}");
+
             }
         }
     }
