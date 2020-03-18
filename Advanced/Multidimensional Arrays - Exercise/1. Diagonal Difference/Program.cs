7f7e7b309dccb780d2d8d7c554792fd2bc274716
tonkatawe <vlahov.anton@gmail.com>
2020-03-18 10:42:41 +0200

    C# Advanced

---
+++ Advanced/Multidimensional Arrays - Exercise/1. Diagonal Difference/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+            var matrix = new int[size, size];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            var firstSum = 0; //save sum of first diagonal
+            var secondSum = 0;//save sum of second diagonal
+            var counter = 0;
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+
+                }
+                firstSum += matrix[row, counter];
+                secondSum += matrix[row, matrix.GetLength(1) - 1 - counter];
+                counter++;
+            }
+
+            var difference = Math.Abs(firstSum - secondSum);
+            Console.WriteLine(difference);
+         
+        }
+    }
+}
---
+++ Advanced/Multidimensional Arrays - Exercise/2. Squares in Matrix/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Squares_in_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            var rows = size[0];
+            var cols = size[1];
+            var matrix = new string[rows, cols];
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currRow = Console.ReadLine()
+                    .Split(' ')
+                    .ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currRow[col].ToString();
+                }
+            }
+
+            var counter = 0; //counter for equal matrix
+            for (int row = 0; row < matrix.GetLength(0) - 1; row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1)-1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1] && matrix[row, col] == matrix[row+1, col+1] && matrix[row, col] == matrix[row+1, col])
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+
+
+    }
+}
