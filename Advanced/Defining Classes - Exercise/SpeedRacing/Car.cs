e1b88d504b85fa0b1b70cc53bb661729db8af806
tonkatawe <vlahov.anton@gmail.com>
2020-04-01 15:20:01 +0300

    C# Advanced

---
+++ Advanced/Defining Classes - Exercise/SpeedRacing/Car.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpeedRacing
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double traveledDistance;
+
+        public Car()
+        {
+            this.TraveledDistance = 0;
+        }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        : this()
+        {
+            this.Model = model;
+            this.FuelAmount = fuelAmount;
+            this.FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+        public string Model
+        {
+            get
+            {
+                return this.model;
+            }
+            set
+            {
+                this.model = value;
+            }
+        }
+
+        public double FuelAmount
+        {
+            get
+            {
+                return this.fuelAmount;
+            }
+            set
+            {
+                this.fuelAmount = value;
+            }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get
+            {
+                return this.fuelConsumptionPerKilometer;
+            }
+            set
+            {
+                this.fuelConsumptionPerKilometer = value;
+            }
+        }
+
+        public double TraveledDistance
+        {
+            get
+            {
+                return this.traveledDistance;
+            }
+            set
+            {
+                this.traveledDistance = value;
+            }
+        }
+
+        public void CarDriveDistance(double amountOfKm)
+        {
+            var neededFuel = amountOfKm * this.FuelConsumptionPerKilometer;
+            if (neededFuel <= this.FuelAmount)
+            {
+                this.fuelAmount -= neededFuel;
+                this.traveledDistance += amountOfKm;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
---
+++ Advanced/Defining Classes - Exercise/SpeedRacing/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SpeedRacing
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());//read number of cars
+            var cars = new HashSet<Car>();
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine().Split();
+                var model = input[0];
+                var fuelAmount = double.Parse(input[1]);
+                var fuelConsumptionPerKilometer = double.Parse(input[2]);
+                var currentCar = new Car(model, fuelAmount, fuelConsumptionPerKilometer);
+                cars.Add(currentCar);
+
+
+            }
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+
+                var tokens = input.Split();
+                var model = tokens[1];
+                var amountOfKm = double.Parse(tokens[2]);
+                foreach (var car in cars)
+                {
+                    if (car.Model == model)
+                    {
+                        car.CarDriveDistance(amountOfKm);
+                    }
+                }
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:F2} {car.TraveledDistance}");
+            }
+        }
+    }
+}
