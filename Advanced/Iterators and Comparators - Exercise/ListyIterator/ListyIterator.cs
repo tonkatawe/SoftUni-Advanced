16002353a7229d77a3da832f960b0f5f999267d5
tonkatawe <vlahov.anton@gmail.com>
2020-04-10 10:43:16 +0300

    C# Advanced

---
+++ Advanced/Iterators and Comparators - Exercise/ListyIterator/ListyIterator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Microsoft.VisualBasic;
+
+namespace ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> items;
+        private int index;
+        public ListyIterator(List<T> items)
+        {
+            this.items = items;
+            this.index = 0;
+        }
+
+        public void Print()
+        {
+            if (!items.Any())
+            {
+                throw new InvalidOperationException("Invalid operation!");
+            }
+
+            Console.WriteLine(items[this.index]);
+        }
+        public bool Move()
+        {
+            if (HasNext())
+            {
+                this.index++;
+                return true;
+            }
+            return false;
+        }
+
+        public bool HasNext()
+        {
+            if (this.index < this.items.Count -1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Advanced/Iterators and Comparators - Exercise/ListyIterator/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace ListyIterator
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var tokens = input
+                .Split()
+                .Skip(1)
+                .ToList();
+
+            ListyIterator<string> list = new ListyIterator<string>(tokens);
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "END")
+                {
+                    break;
+                }
+                else if (command == "Move")
+                {
+                    Console.WriteLine(list.Move());
+                    
+                }
+                else if (command == "Print")
+                {
+                    try
+                    {
+                        list.Print();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                  
+                    }
+                }
+                else if (command == "HasNext")
+                {
+                    Console.WriteLine(list.HasNext());
+                   
+                }
+            }
+
+        }
+    }
+}
