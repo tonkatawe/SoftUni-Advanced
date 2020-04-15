5ca61d4a373fd37302d42390a200f26425bea635
tonkatawe <vlahov.anton@gmail.com>
2020-04-15 13:20:34 +0300

    C# Advanced

--- Advanced/Exams/Advanced - Exams/01. Santa's Present Factory/Program.cs
+++ Advanced/Exams/Advanced - Exams/01. Santa's Present Factory/Program.cs
@@ -13,39 +13,87 @@ namespace _01._Santa_s_Present_Factory
             var m = ParseArrayFromConsole();
             var boxesWithMatirial = new Stack<int>(n);
             var magicValues = new Queue<int>(m);
-
-            while (boxesWithMatirial.Count !=0 && magicValues.Count !=0)
+            var data = new SortedDictionary<string, int>();
+            data["Doll"] = 0;
+            data["Wooden train"] = 0;
+            data["Teddy bear"] = 0;
+            data["Bicycle"] = 0;
+            while (boxesWithMatirial.Count != 0 && magicValues.Count != 0)
             {
                 var currentMagic = boxesWithMatirial.Peek() * magicValues.Peek();
-                if (currentMagic == 150 || currentMagic == 250 || currentMagic == 300 || currentMagic ==400)
+                if (currentMagic == 150 || currentMagic == 250 || currentMagic == 300 || currentMagic == 400)
                 {
                     boxesWithMatirial.Pop();
                     magicValues.Dequeue();
+                    switch (currentMagic)
+                    {
+                        case 150:
+                            data["Doll"]++;
+                            break;
+                        case 250:
+                            data["Wooden train"]++;
+                            break;
+                        case 300:
+                            data["Teddy bear"]++;
+                            break;
+                        case 400:
+                            data["Bicycle"]++;
+                            break;
+                        default:
+                            break;
+                    }
+
                 }
-                else if (currentMagic <0)
+                else if (currentMagic < 0)
                 {
                     var sum = boxesWithMatirial.Pop() + magicValues.Dequeue();
                     boxesWithMatirial.Push(sum);
                 }
-                else if (currentMagic >0)
+                else if (currentMagic > 0)
                 {
                     magicValues.Dequeue();
                     var current = boxesWithMatirial.Pop() + 15;
                     boxesWithMatirial.Push(current);
                 }
 
-                if (boxesWithMatirial.Peek() == 0)
+                else if (boxesWithMatirial.Peek() == 0 || magicValues.Peek() == 0)
                 {
-                    boxesWithMatirial.Pop();
+                    if (boxesWithMatirial.Peek() == 0)
+                    {
+                        boxesWithMatirial.Pop();
+                    }
+                    if (magicValues.Peek() == 0)
+                    {
+                        magicValues.Dequeue();
+                    }
                 }
 
-                if (magicValues.Peek() == 0)
-                {
-                    magicValues.Dequeue();
-                }
 
             }
 
+            if ((data["Doll"] > 0 && data["Wooden train"] > 0) || (data["Teddy bear"] > 0 && data["Bicycle"] > 0))
+            {
+                Console.WriteLine("The presents are crafted! Merry Christmas!");
+            }
+            else
+            {
+                Console.WriteLine("No presents this Christmas!");
+            }
+
+            if (boxesWithMatirial.Any())
+            {
+                Console.WriteLine($"Materials left: {string.Join(", ", boxesWithMatirial)}");
+            }
+
+            if (magicValues.Any())
+            {
+                Console.WriteLine($"Magic left: {string.Join(", ", magicValues)}");
+            }
+
+            foreach (var toy in data.Where(x => x.Value > 0))
+            {
+                Console.WriteLine($"{toy.Key}: {toy.Value}");
+            }
         }
         public static int[] ParseArrayFromConsole()
         {
---
+++ Advanced/Exams/Advanced - Exams/02. Present Delivery/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Present_Delivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var m = int.Parse(Console.ReadLine());
+            var countOfPresents = m;
+            var n = int.Parse(Console.ReadLine()); //read size of square matrix
+            var matrix = new string[n, n];
+            //use next two variables to save Santa`s location
+            var rowStart = 0;
+            var colStart = 0;
+            //make counter to save all nice kids
+            var niceKidsCounter = 0;
+            //make counter for all nice kids in the matrix
+            var matrixNiceKids = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currentRow = Console.ReadLine()
+                    .Split()
+                    .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                    if (matrix[row, col] == "S")
+                    {
+                        rowStart = row;
+                        colStart = col;
+                    }
+
+                    if (currentRow[col] == "V")
+                    {
+                        matrixNiceKids++;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "Christmas morning" || countOfPresents == 0)
+                {
+                    break;
+                }
+                //I am not sure but might be I have to make 'if' for matrix`s boundary per each movement. Firstly I`ll skip this.
+                else if (command == "up")
+                {
+                    matrix[rowStart, colStart] = "-";
+                    rowStart--;
+                    //    TaskLogical(matrix, rowStart, colStart, countOfPresents);
+                    if (matrix[rowStart, colStart] == "X")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                    }
+                    else if (matrix[rowStart, colStart] == "V")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                        countOfPresents--;
+                    }
+                    else if (matrix[rowStart, colStart] == "C")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (matrix[rowStart + 1, colStart] == "X" || matrix[rowStart + 1, colStart] == "V")
+                            {
+                                matrix[rowStart + 1, colStart] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart - 1, colStart] == "X" || matrix[rowStart - 1, colStart] == "V")
+                            {
+                                matrix[rowStart - 1, colStart] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart, colStart + 1] == "X" || matrix[rowStart, colStart + 1] == "V")
+                            {
+                                matrix[rowStart, colStart + 1] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart, colStart - 1] == "X" || matrix[rowStart, colStart - 1] == "V")
+                            {
+                                matrix[rowStart, colStart - 1] = "-";
+                                countOfPresents--;
+                            }
+
+                            if (countOfPresents == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if (command == "down")
+                {
+                    matrix[rowStart, colStart] = "-";
+                    rowStart++;
+                    //     TaskLogical(matrix, rowStart, colStart, countOfPresents);
+                    if (matrix[rowStart, colStart] == "X")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                    }
+                    else if (matrix[rowStart, colStart] == "V")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                        countOfPresents--;
+                    }
+                    else if (matrix[rowStart, colStart] == "C")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (matrix[rowStart + 1, colStart] == "X" || matrix[rowStart + 1, colStart] == "V")
+                            {
+                                matrix[rowStart + 1, colStart] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart - 1, colStart] == "X" || matrix[rowStart - 1, colStart] == "V")
+                            {
+                                matrix[rowStart - 1, colStart] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart, colStart + 1] == "X" || matrix[rowStart, colStart + 1] == "V")
+                            {
+                                matrix[rowStart, colStart + 1] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart, colStart - 1] == "X" || matrix[rowStart, colStart - 1] == "V")
+                            {
+                                matrix[rowStart, colStart - 1] = "-";
+                                countOfPresents--;
+                            }
+
+                            if (countOfPresents == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if (command == "left")
+                {
+                    matrix[rowStart, colStart] = "-";
+                    colStart--;
+                    //   TaskLogical(matrix, rowStart, colStart, countOfPresents);
+                    if (matrix[rowStart, colStart] == "X")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                    }
+                    else if (matrix[rowStart, colStart] == "V")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                        countOfPresents--;
+                    }
+                    else if (matrix[rowStart, colStart] == "C")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (matrix[rowStart + 1, colStart] == "X" || matrix[rowStart + 1, colStart] == "V")
+                            {
+                                matrix[rowStart + 1, colStart] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart - 1, colStart] == "X" || matrix[rowStart - 1, colStart] == "V")
+                            {
+                                matrix[rowStart - 1, colStart] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart, colStart + 1] == "X" || matrix[rowStart, colStart + 1] == "V")
+                            {
+                                matrix[rowStart, colStart + 1] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart, colStart - 1] == "X" || matrix[rowStart, colStart - 1] == "V")
+                            {
+                                matrix[rowStart, colStart - 1] = "-";
+                                countOfPresents--;
+                            }
+
+                            if (countOfPresents == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if (command == "right")
+                {
+                    matrix[rowStart, colStart] = "-";
+                    colStart++;
+                    //   TaskLogical(matrix, rowStart, colStart, countOfPresents);
+                    if (matrix[rowStart, colStart] == "X")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                    }
+                    else if (matrix[rowStart, colStart] == "V")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                        countOfPresents--;
+                    }
+                    else if (matrix[rowStart, colStart] == "C")
+                    {
+                        matrix[rowStart, colStart] = "-";
+                        for (int i = 0; i < 4; i++)
+                        {
+                            if (matrix[rowStart + 1, colStart] == "X" || matrix[rowStart + 1, colStart] == "V")
+                            {
+                                matrix[rowStart + 1, colStart] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart - 1, colStart] == "X" || matrix[rowStart - 1, colStart] == "V")
+                            {
+                                matrix[rowStart - 1, colStart] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart, colStart + 1] == "X" || matrix[rowStart, colStart + 1] == "V")
+                            {
+                                matrix[rowStart, colStart + 1] = "-";
+                                countOfPresents--;
+                            }
+                            else if (matrix[rowStart, colStart - 1] == "X" || matrix[rowStart, colStart - 1] == "V")
+                            {
+                                matrix[rowStart, colStart - 1] = "-";
+                                countOfPresents--;
+                            }
+
+                            if (countOfPresents == 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            matrix[rowStart, colStart] = "S";
+            if (countOfPresents == 0)
+            {
+                Console.WriteLine("Santa ran out of presents!");
+            }
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                    if (matrix[row, col] == "V")
+                    {
+                        niceKidsCounter++;
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+            if (niceKidsCounter > 0 && countOfPresents == 0)
+            {
+                Console.WriteLine($"No presents for {niceKidsCounter} nice kid/s.");
+            }
+            else
+            {
+                Console.WriteLine($"Good job, Santa! {matrixNiceKids} happy nice kid/s.");
+            }
+        }
+    }
+}
