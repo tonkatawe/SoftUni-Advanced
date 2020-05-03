5f32aef4312d242555c50e911645c381a8701ed3
tonkatawe <vlahov.anton@gmail.com>
2020-05-03 20:53:06 +0300

    C# Advanced

--- Advanced/Exams/Advanced - Exams/04. Travel Map/Program.cs
+++ Advanced/Exams/Advanced - Exams/04. Travel Map/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace _04._Travel_Map
@@ -7,6 +9,7 @@ namespace _04._Travel_Map
     {
         static void Main(string[] args)
         {
+            var data = new Dictionary<string, Dictionary<string, int>>();
             while (true)
             {
                 var command = Console.ReadLine();
@@ -19,6 +22,30 @@ namespace _04._Travel_Map
                 var country = tokens[0];
                 var town = tokens[1];
                 var cost = int.Parse(tokens[2]);
+                if (!data.ContainsKey(country))
+                {
+                    data[country] = new Dictionary<string, int>();
+                }
+
+                if (!data[country].ContainsKey(town))
+                {
+                    data[country][town] = cost;
+                }
+                if (data[country].ContainsKey(town) && data[country][town] > cost)
+                {
+                    data[country][town] = cost;
+                }
+            }
+
+            foreach (var country in data.OrderBy(x=> x.Key))
+            {
+                Console.Write($"{country.Key} -> ");
+                foreach (var town in country.Value.OrderBy(x=>x.Value))
+                {
+                    Console.Write($"{town.Key} -> {town.Value} ");
+                }
+
+                Console.WriteLine();
             }
         }
     }
