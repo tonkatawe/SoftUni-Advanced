62987ef61608cc4086bee954cfe4fa2e750b4312
tonkatawe <vlahov.anton@gmail.com>
2020-05-18 19:05:04 +0300

    C# Advanced

---
+++ Advanced/Exams/Advanced - Exams/02. Tagram/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Tagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new Dictionary<string, Dictionary<string, int>>();
+            while (true)
+            {
+                var command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+
+                if (command.Contains("->"))
+                {
+                    var tokens = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                    var username = tokens[0];
+                    var tag = tokens[1];
+                    var likes = int.Parse(tokens[2]);
+                    if (!data.ContainsKey(username) && likes >=0)
+                    {
+                        data[username] = new Dictionary<string, int>();
+
+                    }
+
+                    if (data.ContainsKey(username) && data[username].ContainsKey(tag) && likes > 0)
+                    {
+                        data[username][tag] += likes;
+                    }
+                    else if (likes > 0)
+                    {
+                        data[username][tag] = likes;
+                    }
+
+                }
+                else
+                {
+                    var userForBan = command.Split(' ').Skip(1).ToArray();
+                    if (data.ContainsKey(userForBan[0]))
+                    {
+                        data.Remove(userForBan[0]);
+                    }
+                }
+            }
+
+            foreach (var user in data.OrderByDescending(x => x.Value.Values.Sum()).ThenBy(x => x.Value.Values.Count()))
+            {
+                Console.WriteLine(user.Key);
+                foreach (var tag in user.Value)
+                {
+                    Console.WriteLine($"- {tag.Key}: {tag.Value}");
+                }
+            }
+        }
+    }
+
+
+}
---
+++ Advanced/Exams/Advanced - Exams/03. Miner/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var size = int.Parse(Console.ReadLine());
+            var matrix = new string[size, size];
+            var commands = ReadArrayFromConsole();
+            var coalCount = 0;
+            var rowStart = 0;
+            var colStart = 0;
+            var minerCoal = 0;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var currentRow = ReadArrayFromConsole();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                    if (currentRow[col] == "s")
+                    {
+                        rowStart = row;
+                        colStart = col;
+                    }
+
+                    if (currentRow[col] == "c")
+                    {
+                        coalCount++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                if (commands[i] == "up")
+                {
+                    if (rowStart - 1 >= 0)
+                    {
+                        rowStart--;
+                        if (matrix[rowStart, colStart] == "e")
+                        {
+                            Console.WriteLine($"Game over! ({rowStart}, {colStart})");
+                            return;
+                        }
+                        else if (matrix[rowStart, colStart] == "c")
+                        {
+                            minerCoal++;
+                            coalCount--;
+                            matrix[rowStart, colStart] = "*";
+                        }
+                    }
+                }
+                else if (commands[i] == "down")
+                {
+                    if (rowStart + 1 < size)
+                    {
+                        rowStart++;
+                        if (matrix[rowStart, colStart] == "e")
+                        {
+                            Console.WriteLine($"Game over! ({rowStart}, {colStart})");
+                            return;
+                        }
+                        else if (matrix[rowStart, colStart] == "c")
+                        {
+                            minerCoal++;
+                            coalCount--;
+                            matrix[rowStart, colStart] = "*";
+                        }
+                    }
+                }
+                else if (commands[i] == "left")
+                {
+                    if (colStart - 1 >= 0)
+                    {
+                        colStart--;
+                        if (matrix[rowStart, colStart] == "e")
+                        {
+                            Console.WriteLine($"Game over! ({rowStart}, {colStart})");
+                            return;
+                        }
+                        else if (matrix[rowStart, colStart] == "c")
+                        {
+                            minerCoal++;
+                            coalCount--;
+                            matrix[rowStart, colStart] = "*";
+                        }
+                    }
+                }
+                else if (commands[i] == "right")
+                {
+                    if (colStart + 1 < size)
+                    {
+                        colStart++;
+                        if (matrix[rowStart, colStart] == "e")
+                        {
+                            Console.WriteLine($"Game over! ({rowStart}, {colStart})");
+                            return;
+                        }
+                        else if (matrix[rowStart, colStart] == "c")
+                        {
+                            minerCoal++;
+                            coalCount--;
+                            matrix[rowStart, colStart] = "*";
+                        }
+                    }
+                }
+
+                if (coalCount == 0)
+                {
+                    Console.WriteLine($"You collected all coals! ({rowStart}, {colStart})");
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{coalCount} coals left. ({rowStart}, {colStart})");
+        }
+
+        private static string[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+        }
+    }
+}
