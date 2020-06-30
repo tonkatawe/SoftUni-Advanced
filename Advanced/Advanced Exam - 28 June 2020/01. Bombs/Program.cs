966193c4ba58f5be1ea7fbf07637527a52c2332e
tonkatawe <vlahov.anton@gmail.com>
2020-06-30 11:52:14 +0300

    C# Advanced

---
+++ Advanced/Advanced Exam - 28 June 2020/01. Bombs/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Bombs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new SortedDictionary<string, int>()
+            {
+                {"Datura Bombs",0},
+                {"Cherry Bombs",0},
+                {"Smoke Decoy Bombs",0}
+            };
+            var n = ReadArrFromConsole();// bomb effects
+            var m = ReadArrFromConsole(); // bomb casing
+
+            var effects = new Queue<int>(n);
+            var casing = new Stack<int>(m);
+
+            while (effects.Any() && casing.Any())
+            {
+                var currentEffects = effects.Peek();
+                var currentCasing = casing.Peek();
+                var sum = currentCasing + currentEffects;
+                if ((data["Smoke Decoy Bombs"] >= 3 && data["Cherry Bombs"] >= 3 && data["Datura Bombs"] >= 3))
+                {
+                    break;
+
+                }
+                if (sum == 40 || sum == 60 || sum == 120)
+                {
+                    if (sum == 40)
+                    {
+                        data["Datura Bombs"]++;
+
+                    }
+                    else if (sum == 60)
+                    {
+                        data["Cherry Bombs"]++;
+                    }
+                    else if (sum == 120)
+                    {
+                        data["Smoke Decoy Bombs"]++;
+                    }
+
+                    casing.Pop();
+                    effects.Dequeue();
+                }
+                else
+                {
+                    casing.Push(casing.Pop() - 5);
+                }
+            }
+
+            if (!effects.Any() || !casing.Any() && (data["Smoke Decoy Bombs"] < 3 || data["Cherry Bombs"] < 3 || data["Datura Bombs"] < 3))
+            {
+                Console.WriteLine("You don't have enough materials to fill the bomb pouch.");
+                if (effects.Any())
+                {
+                    Console.WriteLine($"Bomb Effects: {string.Join(", ", effects)}");
+                }
+                else
+                {
+                    Console.WriteLine($"Bomb Effects: empty");
+                }
+
+                if (casing.Any())
+                {
+                    Console.WriteLine($"Bomb Casings: {string.Join(", ", casing)}");
+                }
+                else
+                {
+
+                    Console.WriteLine($"Bomb Casings: empty");
+                }
+                foreach (var bomb in data)
+                {
+                    Console.WriteLine($"{bomb.Key}: {bomb.Value}");
+                }
+            }
+            else if ((data["Smoke Decoy Bombs"] >= 3 && data["Cherry Bombs"] >= 3 && data["Datura Bombs"] >= 3))
+            {
+
+                Console.WriteLine("Bene! You have successfully filled the bomb pouch!");
+                if (effects.Any())
+                {
+                    Console.WriteLine($"Bomb Effects: {string.Join(", ", effects)}");
+                }
+                else
+                {
+                    Console.WriteLine($"Bomb Effects: empty");
+                }
+
+                if (casing.Any())
+                {
+                    Console.WriteLine($"Bomb Casings: {string.Join(", ", casing)}");
+                }
+                else
+                {
+
+                    Console.WriteLine($"Bomb Casings: empty");
+                }
+                foreach (var bomb in data)
+                {
+                    Console.WriteLine($"{bomb.Key}: {bomb.Value}");
+                }
+            }
+
+        }
+
+        private static int[] ReadArrFromConsole()
+        {
+            return Console.ReadLine()
+                .Split(", ")
+                .Select(int.Parse)
+                .ToArray();
+        }
+    }
+}
---
+++ Advanced/Advanced Exam - 28 June 2020/02. Snake/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var matrix = new char[n, n];
+            var row = 0;
+            var col = 0;
+            var food = 0;
+
+            var data = new Queue<Tuple<int, int>>();
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                var current = Console.ReadLine().ToCharArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = current[j];
+                    if (current[j] == 'S')
+                    {
+                        row = i;
+                        col = j;
+                    }
+
+                    if (current[j] == 'B')
+                    {
+                        data.Enqueue(new Tuple<int, int>(i, j));
+                    }
+                }
+            }
+
+            while (true)
+            {
+                if (food >= 10)
+                {
+                    Console.WriteLine("You won! You fed the snake.");
+                    Console.WriteLine($"Food eaten: {food}");
+                 //   matrix[row, col] = 'S';
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matrix.GetLength(1); j++)
+                        {
+                            Console.Write($"{matrix[i, j]}");
+                        }
+
+                        Console.WriteLine();
+                    }
+                    return;
+
+                }
+                var path = Console.ReadLine();
+
+                if (path == "up" && row - 1 >= 0)
+                {
+                    matrix[row, col] = '.';
+                    row--;
+                    if (matrix[row, col] == '*')
+                    {
+                        food++;
+                       // matrix[row, col] = '.';
+                    }
+
+                    if (matrix[row, col] == 'B')
+                    {
+                        matrix[row, col] = '.';
+                        data.Dequeue();
+                        var newrow = data.Dequeue();
+                        matrix[row, col] = '.';
+                        row = newrow.Item1;
+                        col = newrow.Item2;
+                        matrix[row, col] = '.';
+
+                    }
+                    matrix[row, col] = 'S';
+                }
+                else if (path == "down" && row+1 < n )
+                {
+                    matrix[row, col] = '.';
+                    row++;
+                    if (matrix[row, col] == '*')
+                    {
+                        food++;
+              //          matrix[row, col] = '.';
+                    }
+
+                    if (matrix[row, col] == 'B')
+                    {
+                        matrix[row, col] = '.';
+                        data.Dequeue();
+                        var newrow = data.Dequeue();
+                        matrix[row, col] = '.';
+                        row = newrow.Item1;
+                        col = newrow.Item2;
+                        matrix[row, col] = '.';
+                    }
+                    matrix[row, col] = 'S';
+                }
+                else if (path == "left" && col-1 >= 0)
+                {
+                    matrix[row, col] = '.';
+                    col--;
+                    if (matrix[row, col] == '*')
+                    {
+                        food++;
+                    //    matrix[row, col] = '.';
+                    }
+
+                    if (matrix[row, col] == 'B')
+                    {
+                        matrix[row, col] = '.';
+                        data.Dequeue();
+                        var newrow = data.Dequeue();
+                        matrix[row, col] = '.';
+                        row = newrow.Item1;
+                        col = newrow.Item2;
+                        matrix[row, col] = '.';
+                    }
+                    matrix[row, col] = 'S';
+                }
+                else if (path == "right" && col+1 < n )
+                {
+                    matrix[row, col] = '.';
+                    col++;
+                    if (matrix[row, col] == '*')
+                    {
+                        food++;
+                   //     matrix[row, col] = '.';
+                    }
+
+                    if (matrix[row, col] == 'B')
+                    {
+                        matrix[row, col] = '.';
+                        data.Dequeue();
+                        var newrow = data.Dequeue();
+                        matrix[row, col] = '.';
+                        row = newrow.Item1;
+                        col = newrow.Item2;
+                        matrix[row, col] = '.';
+                    }
+                    matrix[row, col] = 'S';
+                }
+                else
+                {
+                    matrix[row, col] = '.';
+                    Console.WriteLine("Game over!");
+                    Console.WriteLine($"Food eaten: {food}");
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        for (int j = 0; j < matrix.GetLength(1); j++)
+                        {
+                            Console.Write($"{matrix[i, j]}");
+                        }
+
+                        Console.WriteLine();
+                    }
+                    return;
+
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
